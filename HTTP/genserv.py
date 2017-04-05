adb40cff56ce7387e9aadb0a63d3c6da1e44e518
jgyates <jgyates@users.noreply.github.com>
2017-04-05 19:09:34 -0400

    Add files via upload
    Added new CSS color for service due

--- HTTP/genserv.py
+++ HTTP/genserv.py
@@ -9,126 +9,9 @@
 # MODIFICATIONS:
 #------------------------------------------------------------
 
-#
-# This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 from flask import Flask, render_template, request, jsonify
 import sys, signal, os, socket, atexit, ConfigParser
-import mylog
-
-
-#----------  ClientInterface::init--- ------------------------------------------
-class ClientInterface:
-    def __init__(self, host="127.0.0.1", port=9082):
-
-        self.EndOfMessage = "EndOfMessage"
-        self.rxdatasize = 2000
-        self.host = host
-        self.port = port
-
-        self.Connect()
-
-    #----------  ClientInterface::Connect ---------------------------------
-    def Connect(self):
-
-        try:
-            #create an INET, STREAMing socket
-            self.Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            #now connect to the server on our port
-            self.Socket.connect((self.host, self.port))
-            RetData, data = self.Receive(noeom = True)       # Get initial status before commands are sent
-            print data
-        except Exception, e1:
-
-            self.FatalError("Error: Connect" + str(e1))
-
-
-    #----------  ClientInterface::SendCommand ---------------------------------
-    def SendCommand(self, cmd):
-
-        try:
-            self.Socket.sendall(cmd)
-        except Exception, e1:
-            self.LogError( "Error: TX:" + str(e1))
-            self.Close()
-            self.Connect()
-
-    #----------  ClientInterface::Receive ---------------------------------
-    def Receive(self, noeom = False):
-
-        RetStatus = True
-        try:
-            data = self.Socket.recv(self.rxdatasize)
-            if len(data):
-                if not self.CheckForStarupMessage(data) or not noeom:
-                    while not self.EndOfMessage in data:
-                        more = self.Socket.recv(self.rxdatasize)
-                        if len(more):
-                            if self.CheckForStarupMessage(more):
-                                data = ""
-                                RetStatus = False
-                                break
-                            data += more
-
-                    if data.endswith(self.EndOfMessage):
-                        data = data[:-len(self.EndOfMessage)]
-                        RetStatus = True
-
-        except Exception, e1:
-            self.LogError( "Error: RX:" + str(e1))
-            self.Close()
-            self.Connect()
-            RetStatus = False
-            data = "Retry"
-
-        return RetStatus, data
-
-    #----------  ClientInterface::CheckForStarupMessage ---------------------------------
-    def CheckForStarupMessage(self, data):
-
-        # check for initial status response from monitor
-        if data.startswith("OK") or data.startswith("CRITICAL:") or data.startswith("WARNING:"):
-            return True
-        else:
-            return False
-
-    #----------  ClientInterface::Close ---------------------------------
-    def Close(self):
-        self.Socket.close()
-
-    #----------  ClientInterface::ProcessMonitorCommand ---------------------------------
-    def ProcessMonitorCommand(self, cmd):
-
-        data = ""
-        try:
-            RetStatus = False
-            while RetStatus == False:
-                self.SendCommand(cmd)
-                RetStatus, data = self.Receive()
-        except Exception, e1:
-            self.LogError("Error in ProcessMonitorCommand:" + str(e1))
-        return data
-
-    #---------------------ClientInterface::FatalError------------------------
-    def LogError(self, Message):
-        log.error(Message)
-
-    #----------  ClientInterface::FatalError ---------------------------------
-    def FatalError(self, Message):
-
-        log.error(Message)
-        raise Exception(Message)
+import mylog, myclient
 
 #------------------------------------------------------------
 app = Flask(__name__,static_url_path='')
@@ -145,7 +28,7 @@ def root():
 @app.route("/cmd/<command>")
 def command(command):
 
-    if command in ["status", "outage", "maint", "logs", "monitor", "getbase", "getsitename", "setexercise", "setquiet", "getexercise"]:
+    if command in ["status", "outage", "maint", "logs", "monitor", "getbase", "getsitename", "setexercise", "setquiet", "getexercise","setremote"]:
         finalcommand = "generator: " + command
         try:
             if command == "setexercise":
@@ -171,6 +54,7 @@ if __name__ == "__main__":
 
     clientport = 0
     try:
+
         config = ConfigParser.RawConfigParser()
         # config parser reads from current directory, when running form a cron tab this is
         # not defined so we specify the full path
@@ -181,7 +65,7 @@ if __name__ == "__main__":
     except Exception, e1:
         log.error("Missing config file or config file entries: " + str(e1))
 
-    MyClientInterface = ClientInterface(port=clientport)
+    MyClientInterface = myclient.ClientInterface(port=clientport, log = log)
     while True:
         try:
             app.run(host="0.0.0.0", port=8000)
