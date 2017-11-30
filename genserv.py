755a872aed70b9f3e9b45128882a27466a9887e5
jgyates <jgyates@users.noreply.github.com>
2017-11-29 19:05:26 -0500

    Add files via upload
    Added optional username / password login

--- genserv.py
+++ genserv.py
@@ -9,7 +9,7 @@
 # MODIFICATIONS:
 #------------------------------------------------------------
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, session
 import sys, signal, os, socket, atexit, configparser
 import mylog, myclient
 
@@ -18,37 +18,68 @@ app = Flask(__name__,static_url_path='')
 
 # log errors in this module to a file
 log = mylog.SetupLogger("genserv", "/var/log/genserv.log")
+HTTPAuthUser = None
+HTTPAuthPass = None
 
 #------------------------------------------------------------
-@app.route('/')
+@app.route('/', methods=['GET'])
 def root():
-    return app.send_static_file('index.html')
+
+    if HTTPAuthUser != None and HTTPAuthPass != None:
+        if not session.get('logged_in'):
+            return render_template('login.html')
+        else:
+            return app.send_static_file('index.html')
+    else:
+        return app.send_static_file('index.html')
+    
+#------------------------------------------------------------
+@app.route('/', methods=['POST'])
+def do_admin_login():
+
+    if request.form['password'] == HTTPAuthPass and request.form['username'] == HTTPAuthUser:
+        session['logged_in'] = True
+        return root()
+    else:
+        return render_template('login.html')
 
 #------------------------------------------------------------
 @app.route("/cmd/<command>")
 def command(command):
 
-    if command in ["status", "outage", "maint", "logs", "monitor", "getbase", "getsitename", "setexercise", "setquiet", "getexercise","setremote", "settime"]:
-        finalcommand = "generator: " + command
-        try:
-            if command == "setexercise":
-                settimestr = request.args.get('setexercise', 0, type=str)
-                finalcommand += "=" + settimestr
-            if command == "setquiet":
-                setquietstr = request.args.get('setquiet', 0, type=str)
-                finalcommand += "=" + setquietstr
-            if command == "setremote":
-                setremotestr = request.args.get('setremote', 0, type=str)
-                finalcommand += "=" + setremotestr
+    if HTTPAuthUser == None or HTTPAuthPass == None:
+        return ProcessCommand(command)
+        
+    if not session.get('logged_in'):
+               return render_template('login.html')
+    else:
+        return ProcessCommand(command)
 
-            data = MyClientInterface.ProcessMonitorCommand(finalcommand)
-        except Exception as e1:
-            data = "Retry"
-            log.error("Error on command function" + str(e1))
-        return jsonify(data)
+#------------------------------------------------------------
+def ProcessCommand(command):
+
+    if command in ["status", "outage", "maint", "logs", "monitor", "getbase", "getsitename", "setexercise", "setquiet", "getexercise","setremote", "settime"]:
+            finalcommand = "generator: " + command
+            try:
+                if command == "setexercise":
+                    settimestr = request.args.get('setexercise', 0, type=str)
+                    finalcommand += "=" + settimestr
+                if command == "setquiet":
+                    setquietstr = request.args.get('setquiet', 0, type=str)
+                    finalcommand += "=" + setquietstr
+                if command == "setremote":
+                    setremotestr = request.args.get('setremote', 0, type=str)
+                    finalcommand += "=" + setremotestr
+
+                data = MyClientInterface.ProcessMonitorCommand(finalcommand)
+            except Exception as e1:
+                data = "Retry"
+                log.error("Error on command function" + str(e1))
+            return jsonify(data)
 
     else:
         return render_template('command_template.html', command = command)
+
 
 #------------------------------------------------------------
 if __name__ == "__main__":
@@ -60,6 +91,7 @@ if __name__ == "__main__":
         bUseSecureHTTP = False
         bUseSelfSignedCert = True
         SSLContext = None
+        HTTPPort = 8000
         
         config = configparser.RawConfigParser()
         # config parser reads from current directory, when running form a cron tab this is
@@ -72,7 +104,16 @@ if __name__ == "__main__":
         if config.has_option('GenMon', 'usehttps'):
             bUseSecureHTTP = config.getboolean('GenMon', 'usehttps')
 
+        # user name and password require usehttps = True
         if bUseSecureHTTP:
+            if config.has_option('GenMon', 'http_user'):
+                HTTPAuthUser = config.get('GenMon', 'http_user')
+
+            if config.has_option('GenMon', 'http_pass'):
+                HTTPAuthPass = config.get('GenMon', 'http_pass')
+
+        if bUseSecureHTTP:
+            app.secret_key = os.urandom(12)
             HTTPPort = 443
             if config.has_option('GenMon', 'useselfsignedcert'):
                 bUseSelfSignedCert = config.getboolean('GenMon', 'useselfsignedcert')
