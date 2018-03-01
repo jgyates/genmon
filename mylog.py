6abda391f049c121b8781f1932cad859a3afddf1
jgyates <jgyates@users.noreply.github.com>
2018-02-28 23:08:29 -0500

    Add files via upload
    minor tweak: increase log size from 4k to 10k

--- mylog.py
+++ mylog.py
@@ -5,20 +5,6 @@
 #
 # MODIFICATIONS:
 #------------------------------------------------------------
-#
-# This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import logging, logging.handlers
 
 #---------- SetupLogger -------------------------
@@ -31,7 +17,7 @@ def SetupLogger(logger_name, log_file, level=logging.INFO, stream = False):
 
     if log_file != "":
 
-        rotate = logging.handlers.RotatingFileHandler(log_file, mode='a',maxBytes=4000,backupCount=5)
+        rotate = logging.handlers.RotatingFileHandler(log_file, mode='a',maxBytes=10000,backupCount=5)
         rotate.setFormatter(formatter)
         l.addHandler(rotate)
 
