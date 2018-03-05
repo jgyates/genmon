e373b18920d6512090d36f7e6977f6411a02f160
jgyates <jgyates@users.noreply.github.com>
2018-03-05 18:19:25 -0500

    Add files via upload
    Added code to cleanup previous log handlers if called again.

--- mylog.py
+++ mylog.py
@@ -10,8 +10,14 @@ import logging, logging.handlers
 #---------- SetupLogger -------------------------
 def SetupLogger(logger_name, log_file, level=logging.INFO, stream = False):
 
-    l = logging.getLogger(logger_name)
-    l.setLevel(level)
+
+    logger = logging.getLogger(logger_name)
+
+    # remove existing logg handlers
+    for handler in logger.handlers[:]:      # make a copy of the list
+        logger.removeHandler(handler)
+
+    logger.setLevel(level)
 
     formatter = logging.Formatter('%(asctime)s : %(message)s')
 
@@ -19,12 +25,12 @@ def SetupLogger(logger_name, log_file, level=logging.INFO, stream = False):
 
         rotate = logging.handlers.RotatingFileHandler(log_file, mode='a',maxBytes=10000,backupCount=5)
         rotate.setFormatter(formatter)
-        l.addHandler(rotate)
+        logger.addHandler(rotate)
 
     if stream:      # print to screen also?
         streamHandler = logging.StreamHandler()
         streamHandler.setFormatter(formatter)
-        l.addHandler(streamHandler)
+        logger.addHandler(streamHandler)
 
 
     return logging.getLogger(logger_name)
