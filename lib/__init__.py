8b9ad6bd57fd8d981fe3dbcbb5f8330655456e41
jgyates <jgyates@users.noreply.github.com>
2018-04-19 18:55:40 -0400

    Add files via upload

---
+++ lib/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#------------------------------------------------------------
+#    FILE: __init__.py
+# PURPOSE: Base modbus protocol support
+#
+#  AUTHOR: Jason G Yates
+#    DATE: 19-Apr-2018
+#
+# MODIFICATIONS:
+#------------------------------------------------------------
