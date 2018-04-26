a8003d1e356ea07d1e259939328cd46ad4463b44
Jason Yates <jasonyates@Jasons-Computer.local>
2018-04-25 23:24:47 -0400

    bumped version

--- genmon.py
+++ genmon.py
@@ -24,7 +24,7 @@ except ImportError as e:
 from genmonlib import mymail, mylog, mythread, mypipe, mysupport, generac_evolution
 
 
-GENMON_VERSION = "V1.6.8"
+GENMON_VERSION = "V1.6.9"
 
 #------------ Monitor class --------------------------------------------
 class Monitor(mysupport.MySupport):
