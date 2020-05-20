c77f2b579f1e87b1d288745a0dd827516ae6a834
jgyates <jgyates@users.noreply.github.com>
2020-05-19 22:21:24 -0400

    V1.14.09

--- startgenmon.sh
+++ startgenmon.sh
@@ -3,50 +3,13 @@
 genmondir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pycmd="python"
 configpath=""
-
-#-------------------------------------------------------------------------------
-function printhelp() {
-
-  echo ""
-  echo "Usage: "
-  echo ""
-  echo "    startgenmon.sh <options> <action>"
-  echo ""
-  echo "Options:"
-  echo ""
-  echo "  -c           Specifiy full path to config file directory"
-  echo "  -p           Use python 3 instead of python 2.7"
-  echo "  -h           Display help"
-  echo ""
-  echo "Actions:"
-  echo ""
-  echo " start) stop) hardstop) restart) "
-  echo ""
-}
-
-#-------------------------------------------------------------------------------
-# main entry
-
-while getopts ":c:p:h" opt; do
-  case ${opt} in
-    h )
-      printhelp
-      exit 0
-      ;;
-    p )
-      pycmd="python$OPTARG"
-      ;;
-    c )
-      config_path="-c $OPTARG"
-      ;;
-    \? )
-      echo "Invalid Option: -$OPTARG" 1>&2
-      printhelp
-      exit 1
-      ;;
-  esac
-done
-case "$2" in
+if [ -z "$2" ]; then
+  configpath=""
+else
+  configpath="-c $2"
+  echo "Using config in $2"
+fi
+case "$1" in
   start)
     echo "Starting genmon python scripts"
     sudo $pycmd "$genmondir/genloader.py" -s $configpath
