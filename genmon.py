510948fec78354ce5002a0c61c8b6cabd1013800
jgyates <jgyates@users.noreply.github.com>
2021-07-12 17:17:33 -0400

    V1.16.13

--- genmon.py
+++ genmon.py
@@ -402,7 +402,6 @@ class Monitor(MySupport):
             SupportData["StartInfo"] = self.GetStartInfo(NoTile = True)
             if not self.bDisablePlatformStats:
                 SupportData["PlatformStats"] = self.GetPlatformStats()
-            SupportData["Comm Stats"] = self.Controller.GetCommStatus()
             SupportData["Data"] = self.Controller.DisplayRegisters(AllRegs = True, DictOut = True)
             # Raw Modbus data
             SupportData["Registers"] = self.Controller.Registers
@@ -449,6 +448,7 @@ class Monitor(MySupport):
             msgbody += self.printToString(self.ProcessDispatch(self.GetStartInfo(NoTile = True),""))
             if not self.bDisablePlatformStats:
                 msgbody += self.printToString(self.ProcessDispatch({"Platform Stats" : self.GetPlatformStats()},""))
+            msgbody += self.printToString(self.ProcessDispatch({"Comm Stats" : self.Controller.GetCommStatus()},""))
 
             msgbody += self.Controller.DisplayRegisters(AllRegs = True)
 
