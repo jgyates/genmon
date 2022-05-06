c96cc09b6d1fa113924b28cab6a88094c782c693
jgyates <jgyates@users.noreply.github.com>
2022-05-06 11:51:39 -0400

    V1.18.11

--- gengpio.py
+++ gengpio.py
@@ -79,6 +79,7 @@ if __name__=='__main__': # usage program.py [server_address]
 
         # These are the GPIP pins numbers on the Raspberry PI GPIO header
         # https://www.element14.com/community/servlet/JiveServlet/previewBody/73950-102-10-339300/pi3_gpio.png
+        # Commnet out or Uncomment the GPIO and Generator status and alarms you wish to monitor.  Limited on GPIO.
         STATUS_READY = config.ReadValue('STATUS_READY', return_type = int, default = 16)
         STATUS_ALARM = config.ReadValue('STATUS_ALARM', return_type = int, default = 18)
         STATUS_SERVICE = config.ReadValue('STATUS_SERVICE', return_type = int, default = 22)
@@ -99,6 +100,14 @@ if __name__=='__main__': # usage program.py [server_address]
         ER_GOVERNOR = config.ReadValue('ER_GOVERNOR', return_type = int, default = 36)
         ER_WARNING = config.ReadValue('ER_WARNING', return_type = int, default = 32)
 
+        # Other Faults
+        #ER_Controller = config.ReadValue('ER_Controller', return_type = int, default = 3) # Must chose from available GPIO
+        #ER_Ignition = config.ReadValue('ER_Ignition', return_type = int, default = ) # Must chose from available GPIO
+        #ER_Low_Fuel = config.ReadValue('ER_Low_Fuel', return_type = int, default = ) # Must chose from available GPIO
+        #ER_Low_Coolant = config.ReadValue('ER_Low_Coolant', return_type = int, default = ) # Must chose from available GPIO
+        #ER_Battery_Problem = config.ReadValue('ER_Battery_Problem', return_type = int, default = ) # Must chose from available GPIO
+        #ER_Charger = config.ReadValue('ER_Charger', return_type = int, default = ) # Must chose from available GPIO
+
         InitGPIO(STATUS_READY, GPIO.OUT, initial=GPIO.LOW)
         InitGPIO(STATUS_ALARM, GPIO.OUT, initial=GPIO.LOW)
         InitGPIO(STATUS_SERVICE, GPIO.OUT, initial=GPIO.LOW)
@@ -118,6 +127,14 @@ if __name__=='__main__': # usage program.py [server_address]
         InitGPIO(ER_OVERLOAD, GPIO.OUT, initial=GPIO.LOW)
         InitGPIO(ER_GOVERNOR, GPIO.OUT, initial=GPIO.LOW)
         InitGPIO(ER_WARNING, GPIO.OUT, initial=GPIO.LOW)
+
+        # Other Faults
+        #InitGPIO(ER_Controller, GPIO.OUT, initial=GPIO.LOW)
+        #InitGPIO(ER_Ignition, GPIO.OUT, initial=GPIO.LOW)
+        #InitGPIO(ER_Low_Fuel, GPIO.OUT, initial=GPIO.LOW)
+        #InitGPIO(ER_Low_Coolant, GPIO.OUT, initial=GPIO.LOW)
+        #InitGPIO(ER_Battery_Problem, GPIO.OUT, initial=GPIO.LOW)
+        #InitGPIO(ER_Charger, GPIO.OUT, initial=GPIO.LOW)
 
         LastEvent = ""
         LastNetStatus = ""
@@ -178,41 +195,65 @@ if __name__=='__main__': # usage program.py [server_address]
                     data = MyClientInterface.ProcessMonitorCommand("generator: getregvalue=05f1")
                     LastErrorCode = int(data,16)
 
-                    # Overspeed/Underspeed (alarms 1200-1206, 1600-1603)
-                    if 1200 <= LastErrorCode <= 1206 or 1600 <= LastErrorCode <= 1603:
+                    # Overspeed/Underspeed (alarms 1200s, 1600s)
+                    if 1200 <= LastErrorCode <= 1299 or 1600 <= LastErrorCode <= 1699:
                         SetGPIO(ER_SPEED,GPIO.HIGH)
 
-                    # Low Oil (alarm 1300)
-                    if LastErrorCode == 1300:
+                    # Low Oil (alarm 1300s)
+                    if 1300 <= LastErrorCode <= 1399:
                         SetGPIO(ER_LOW_OIL,GPIO.HIGH)
 
-                    # High Temp (alarm 1400)
-                    if LastErrorCode == 1400:
+                    # High Temp (alarm 1400s)
+                    if 1400 <= LastErrorCode <= 1499:
                         SetGPIO(ER_HIGH_TEMP,GPIO.HIGH)
 
-                    # RPM Sensor (alarm 1500-1521)
-                    if 1500 <= LastErrorCode <= 1521:
+                    # RPM Sensor (alarm 1500s)
+                    if 1500 <= LastErrorCode <= 1599:
                         SetGPIO(ER_RPM_SENSE,GPIO.HIGH)
 
-                    # Overvoltage/Undervoltage (alarm 1800-1803, 1900-1906)
-                    if 1800 <= LastErrorCode <= 1803 or 1900 <= LastErrorCode <= 1906:
+                    # Overvoltage/Undervoltage (alarm 1800s, 1900s)
+                    if 1800 <= LastErrorCode <= 1899 or 1900 <= LastErrorCode <= 1999:
                         SetGPIO(ER_VOLTAGE,GPIO.HIGH)
 
-                    # Overcrank (alarm 1100-1101)
-                    if 1100 <= LastErrorCode <= 1101:
+                    # Overcrank (alarm 1100s)
+                    if 1100 <= LastErrorCode <= 1199:
                         SetGPIO(ER_OVERCRANK,GPIO.HIGH)
 
-                    # Overload (alarm 2100-2103)
-                    if 2100 <= LastErrorCode <= 2103:
+                    # Overload (alarm 2100s)
+                    if 2100 <= LastErrorCode <= 2199:
                         SetGPIO(ER_OVERLOAD,GPIO.HIGH)
 
-                    # Governor (alarm 2500-2502)
-                    if 2500 <= LastErrorCode <= 2502:
+                    # Governor (alarm 2500s)
+                    if 2500 <= LastErrorCode <= 2599:
                         SetGPIO(ER_GOVERNOR,GPIO.HIGH)
 
                     # Warning (alarm 0000)
                     if 0000 == LastErrorCode:
                         SetGPIO(ER_WARNING,GPIO.HIGH)
+
+                    # Controller Internal (alarm 1000s)
+                    #if 1000 <= LastErrorCode <= 1099:
+                    #    SetGPIO(ER_Controller,GPIO.HIGH)
+
+                    # Ignition (alarm 2600-2669)
+                    #if 2600 <= LastErrorCode <= 2669:
+                    #    SetGPIO(ER_Ignition,GPIO.HIGH)
+
+                    # Fuel Level (alarm 2680 and 2700)
+                    #if LastErrorCode == 2680 or LastErrorCode == 2700:
+                    #    SetGPIO(ER_Low_Fuel,GPIO.HIGH)
+
+                    # Low Coolant (alarm 2720)
+                    #if LastErrorCode == 2720:
+                    #    SetGPIO(ER_Low_Coolant,GPIO.HIGH)
+
+                    # Battery (alarm 2750 and 2760)
+                    #if LastErrorCode == 2750 or LastErrorCode == 2760:
+                    #    SetGPIO(ER_Battery_Problem,GPIO.HIGH)
+
+                    # Charger (alarm 2770 and 2780)
+                    #if LastErrorCode == 2770 or LastErrorCode == 2780:
+                    #    SetGPIO(ER_Charger,GPIO.HIGH)
 
                 else:
                     SetGPIO(ER_SPEED,GPIO.LOW)
@@ -224,6 +265,12 @@ if __name__=='__main__': # usage program.py [server_address]
                     SetGPIO(ER_OVERLOAD,GPIO.LOW)
                     SetGPIO(ER_GOVERNOR,GPIO.LOW)
                     SetGPIO(ER_WARNING,GPIO.LOW)
+                    #SetGPIO(ER_Controller,GPIO.LOW)
+                    #SetGPIO(ER_Ignition,GPIO.LOW)
+                    #SetGPIO(ER_Low_Fuel,GPIO.LOW)
+                    #SetGPIO(ER_Low_Coolant,GPIO.LOW)
+                    #SetGPIO(ER_Battery_Problem,GPIO.LOW)
+                    #SetGPIO(ER_Charger,GPIO.LOW)
 
             # Get Genmon status
             try:
